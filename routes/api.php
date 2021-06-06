4539517d465856b2b5cac703473638b283d9eaec
sophy8281 <wanjikusophia3@gmail.com>
2021-06-06 20:49:15 +0300

    new api routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Product;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +21,22 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('products', ProductController::class);
+Route::post('/register',[AuthController::class,'register']);
+
+//public routes
+Route::get('/products', [ProductController::class,'index']);
+Route::get('/products/{id}', [ProductController::class,'show']);
+
+//Protected routes-Only authenticated users can have access to protected routes//
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products',[ProductController::class,'store']);
+    Route::put('/products/{id}',[ProductController::class,'update']);
+    Route::delete('/products/{id}',[ProductController::class,'delete']);
+    Route::post('/logout',[AuthController::class,'logout']);
+});
+
+// Route::resource('products', ProductController::class);
+
 // Route::get('/products', [ProductController::class,'index']);
 // Route::post('/products', [ProductController::class,'store']);
 // Route::post('/products', function () {
